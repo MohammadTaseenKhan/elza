6fc1bf163c9a293aa5d69f7358eac56bc96625ae
Basil K Y <techiebasil@gmail.com>
2021-11-15 20:52:29 +0530

    update packages

--- src/functions/getUserAgent.js
+++ src/functions/getUserAgent.js
@@ -2,7 +2,7 @@
 
 var USER_AGENT =
   'Mozilla/5.0 (X11; Linux x86_64; rv:VERSION) Gecko/20100101 Firefox/VERSION'
-var currentVersion = '90.0'
+var currentVersion = '94.0'
 var platform = window.preloadAPI.send('getPlatform', '', true)
 if (platform === 'win32')
   USER_AGENT =
